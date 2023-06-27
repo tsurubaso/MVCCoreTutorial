10d580df4ec506821cab9c9577760e7c739d1fef
tsurubaso <tsurubaso@gmail.com>
2023-06-27 15:04:42 +0900

    Add project files.

---
+++ MVCCoreTutorial/Controllers/PersonController1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCCoreTutorial.Models.Domain;
+
+namespace MVCCoreTutorial.Controllers
+{
+    public class PersonController1 : Controller
+    {
+        public IActionResult Index()
+            
+        {
+            ViewBag.greetings = "hello";
+            ViewData["greetings2"] = "hello2";
+            TempData["greetings3"] = "hello3";
+            return View();
+        }
+
+        public IActionResult AddPerson() 
+        
+        { 
+        
+            return View();
+        
+        }
+        [HttpPost]
+        public IActionResult AddPerson(Person person  ) 
+        
+        {
+
+            if(!ModelState.IsValid) 
+            {
+                return View();
+            
+            }
+            TempData["msg"] = "Added";
+            return View();
+        }
+    }
+}
---
+++ MVCCoreTutorial/Models/Domain/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVCCoreTutorial.Models.Domain
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) 
+        {
+        
+        
+        }
+
+        public DbSet<Person> Persons { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
